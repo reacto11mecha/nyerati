fd7592d4f617ca4bf138f0f2f3d9fc1bb2d70cd6
Ezra Khairan Permana <ezrakhairan123@gmail.com>
2021-02-27 15:37:24 +0700

    Added socket event and ui

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,70 @@
-import { Flex, Button, useColorMode } from "@chakra-ui/react";
+import { useRef, useEffect } from "react";
+import { io } from "socket.io-client";
+import { Flex, Box, HStack, Button, useColorMode } from "@chakra-ui/react";
+
+const socket = io();
 
 export default function Home() {
   const { toggleColorMode } = useColorMode();
+  const ref = useRef();
+
+  useEffect(() => {
+    const curr = ref.current;
+
+    const handleTouchStart = (evt) => {
+      const x = evt.touches[0].clientX;
+      const y = evt.touches[0].clientY;
+
+      const position = {
+        x,
+        y,
+      };
+
+      socket.emit("touchstart", position);
+    };
+    const handleTouchMove = (evt) => {
+      const x = evt.touches[0].clientX;
+      const y = evt.touches[0].clientY;
+
+      const position = {
+        x,
+        y,
+      };
+
+      socket.emit("touchmove", position);
+    };
+
+    curr.addEventListener("touchstart", handleTouchStart, false);
+    curr.addEventListener("touchmove", handleTouchMove, false);
+
+    return () => {
+      curr.removeEventListener("touchstart", handleTouchStart);
+      curr.removeEventListener("touchmove", handleTouchMove);
+    };
+  }, []);
 
   return (
     <Flex width="full" height="100vh" align="center" justifyContent="center">
-      <Button onClick={toggleColorMode} right="0" top="0">
-        Toggle Mode
-      </Button>
+      <Box
+        p={8}
+        ref={ref}
+        top="50%"
+        width="95%"
+        height="95%"
+        boxShadow="lg"
+        borderWidth={1}
+        borderRadius={8}
+        backgroundColor="#fff"
+      >
+        <HStack>
+          <Button onClick={toggleColorMode}>Toggle Dark Mode</Button>
+          <Button
+            onClick={() => ref.current && ref.current.requestFullscreen()}
+          >
+            Fullscreen
+          </Button>
+        </HStack>
+      </Box>
     </Flex>
   );
 }
--- server.js
+++ server.js
@@ -1,12 +1,19 @@
 const socketIO = require("socket.io");
 const express = require("express");
+const robot = require("robotjs");
 const next = require("next");
 const http = require("http");
 
+const gcd = (a, b) => (b == 0 ? a : gcd(b, a % b));
+const screenSize = robot.getScreenSize();
+
+const ratio = gcd(screenSize.width, screenSize.height);
 const port = parseInt(process.env.PORT, 10) || 3000;
 const dev = process.env.NODE_ENV !== "production";
 const app = next({ dev });
 const handle = app.getRequestHandler();
+
+const moveMouse = (data) => robot.moveMouse(data.x * ratio, data.y * ratio);
 
 app.prepare().then(() => {
   const exApp = express();
@@ -24,5 +31,10 @@ app.prepare().then(() => {
 
   const Sock = socketIO(server);
 
-  Sock.on("connection", (socc) => {});
+  Sock.on("connection", (socc) => {
+    console.log("SOCKET CONNECTED");
+
+    socc.on("touchstart", moveMouse);
+    socc.on("touchmove", moveMouse);
+  });
 });
