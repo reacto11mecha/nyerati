bee197f76689ade1a735f365d7a4f615e6a37b14
Ezra Khairan Permana <ezrakhairan123@gmail.com>
2021-04-04 19:02:33 +0700

    Added some styling and stop button

--- components/Player.js
+++ components/Player.js
@@ -1,10 +1,35 @@
-import { Flex, Button } from "@chakra-ui/react";
+import {
+  Flex,
+  VStack,
+  Button,
+  useColorMode,
+  useColorModeValue,
+} from "@chakra-ui/react";
 import useReplay from "./hooks/useReplay";
 import { useRef, useEffect } from "react";
 
 export default function Player({ coord }) {
   const canvasRef = useRef();
-  const { position, playing, toggle } = useReplay(coord, canvasRef);
+  const { colorMode } = useColorMode();
+  const { position, playing, toggle, stop } = useReplay(coord, canvasRef);
+
+  const buttonColor = {
+    play: useColorModeValue("green.300", "green.600"),
+    pause: useColorModeValue("yellow.300", "yellow.700"),
+  };
+  const backgroundFlexColor = useColorModeValue("gray.100", "gray.800");
+  const canvasColor = useColorModeValue("#F7FAFC", "#171923");
+  const stopButton = useColorModeValue("red.400", "red.800");
+
+  useEffect(() => {
+    const canvas = canvasRef.current;
+    const ctx = canvas.getContext("2d");
+
+    console.log(canvasColor);
+
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    ctx.fillStyle = canvasColor;
+  }, [colorMode]);
 
   useEffect(() => {
     const canvas = canvasRef.current;
@@ -14,7 +39,7 @@ export default function Player({ coord }) {
     const y = position.y * canvas.height;
 
     ctx.clearRect(0, 0, canvas.width, canvas.height);
-    ctx.fillStyle = "#171923";
+    ctx.fillStyle = canvasColor;
     ctx.fillRect(0, 0, canvas.width, canvas.height);
     ctx.beginPath();
     ctx.arc(x, y, 10, 0, Math.PI * 2);
@@ -24,20 +49,34 @@ export default function Player({ coord }) {
   }, [position]);
 
   return (
-    <Flex width="full" height="100vh" align="center" justifyContent="center">
+    <Flex
+      width="full"
+      height="100vh"
+      align="center"
+      justifyContent="center"
+      backgroundColor={backgroundFlexColor}
+    >
       <canvas
         ref={canvasRef}
         width="640"
         height="480"
         style={{
-          border: "3.5px solid #171923",
-          backgroundColor: "#171923",
+          border: `3.5px solid ${canvasColor}`,
+          backgroundColor: canvasColor,
           borderRadius: "5px",
         }}
       />
-      <Button onClick={toggle} marginLeft="20px">
-        {playing ? "Pause" : "Play"}
-      </Button>
+      <VStack marginLeft="20px" direction={"row"} align="left">
+        <Button
+          onClick={toggle}
+          backgroundColor={playing ? buttonColor.pause : buttonColor.play}
+        >
+          {playing ? "Pause" : "Play"}
+        </Button>
+        <Button backgroundColor={stopButton} disabled={!playing} onClick={stop}>
+          Stop
+        </Button>
+      </VStack>
     </Flex>
   );
 }
