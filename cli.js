bd341f501f115f9492ed8b09f034fe1bab60ef1e
Ezra Khairan Permana <ezrakhairan123@gmail.com>
2021-05-02 11:10:53 +0700

    update cli app action command

--- cli.js
+++ cli.js
@@ -1,21 +1,35 @@
 #!/usr/bin/env node
 const program = require("commander");
 const package = require("./package.json");
+const { spawn } = require("child_process");
+const { join } = require("path");
 
 program.version(package.version).name(package.name);
+
+const server = join(__dirname, "server.js");
 
 program
   .command("run")
   .alias("r")
   .action(() => {
-    console.log("runned");
+    const child = spawn("node", [server], {
+      env: { ...process.env, NODE_ENV: "production" },
+    });
+
+    child.stderr.pipe(process.stderr);
+    child.stdout.pipe(process.stdout);
   });
 
 program
   .command("record")
   .alias("rec")
   .action(() => {
-    console.log("runned");
+    const child = spawn("node", [server], {
+      env: { ...process.env, NODE_ENV: "production", RECORD: "record" },
+    });
+
+    child.stderr.pipe(process.stderr);
+    child.stdout.pipe(process.stdout);
   });
 
 program.parse(process.argv);
