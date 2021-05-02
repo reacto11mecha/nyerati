164dc5320a18c9019dc3498972072503d687de1b
Ezra Khairan Permana <ezrakhairan123@gmail.com>
2021-05-02 21:16:18 +0700

    Update exec command, added build function, added childHandler stderr, and show help if no command executed

--- cli.js
+++ cli.js
@@ -8,16 +8,19 @@ const chalk = require("chalk");
 const consoleListen = require("./functions/consoleListen");
 const listen = () => consoleListen(parseInt(process.env.PORT, 10) || 3000);
 
-program.version(package.version).name(package.name);
+program.version(package.version).name(Object.keys(package.bin)[0]);
 
-const server = join(__dirname, "server.js");
+const command = "node server.js";
+const cwd = __dirname;
 
 program
   .command("run")
   .alias("r")
+  .description("Running a normal mode of pentab")
   .action(() => {
-    const child = exec(`cd ${__dirname} && node ${server}`, {
+    const child = exec(command, {
       env: { ...process.env, NODE_ENV: "production" },
+      cwd,
     });
 
     childHandler(child);
@@ -26,17 +29,43 @@ program
 program
   .command("record")
   .alias("rec")
+  .description("Running a record mode of pentab")
   .action(() => {
-    const child = exec(`cd ${__dirname} && node ${server}`, {
+    const child = exec(command, {
       env: { ...process.env, NODE_ENV: "production", RECORD: "record" },
+      cwd,
     });
 
     childHandler(child);
   });
 
-function childHandler(child) {
-  child.stderr.pipe(process.stderr);
+let buildAttempt = false;
 
+function build() {
+  if (!buildAttempt) {
+    buildAttempt = true;
+    console.log(
+      `[${chalk.hex("#D1070A")(
+        "Error"
+      )}] Could not find a production build, in an attempt to build it.....`
+    );
+
+    const child = exec("npm run build", {
+      cwd,
+    });
+
+    child.stdout.pipe(process.stdout);
+    child.stderr.pipe(process.stderr);
+
+    child.on("exit", (code) => {
+      if (code === 0) {
+        console.log(`[${chalk.green("Success")}] Success building next-pentab`);
+      }
+    });
+  }
+}
+
+function childHandler(child) {
   child.stdout.on("data", (text) => {
     if (text.includes("next-pentab")) listen();
     if (text.includes("[Socket]")) {
@@ -45,6 +74,21 @@ function childHandler(child) {
       console.log(`[${chalk.hex("#FDD798")("Socket")}] ${event}`);
     }
   });
+
+  child.stderr.on("data", (text) => {
+    if (text.includes("Could not find a production build")) {
+      build();
+    }
+  });
 }
 
-program.parse(process.argv);
+const parsed = program.parse(process.argv);
+if (
+  !(
+    parsed.args &&
+    parsed.args.length > 0 &&
+    typeof (parsed.args[0] === "object")
+  )
+) {
+  program.help();
+}
