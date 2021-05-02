5c6b07a6a31bf1fbf10844306bb23a752d3bbe42
Ezra Khairan Permana <ezrakhairan123@gmail.com>
2021-05-02 21:40:20 +0700

    Splitting the code into separate files | refactor

--- cli.js
+++ cli.js
@@ -1,86 +1,22 @@
 #!/usr/bin/env node
 const program = require("commander");
 const package = require("./package.json");
-const { exec } = require("child_process");
-const { join } = require("path");
-const chalk = require("chalk");
 
-const consoleListen = require("./functions/consoleListen");
-const listen = () => consoleListen(parseInt(process.env.PORT, 10) || 3000);
+const { run, record } = require("./lib/action")(__dirname);
 
 program.version(package.version).name(Object.keys(package.bin)[0]);
-
-const command = "node server.js";
-const cwd = __dirname;
 
 program
   .command("run")
   .alias("r")
   .description("Running a normal mode of pentab")
-  .action(() => {
-    const child = exec(command, {
-      env: { ...process.env, NODE_ENV: "production" },
-      cwd,
-    });
-
-    childHandler(child);
-  });
+  .action(() => run());
 
 program
   .command("record")
   .alias("rec")
   .description("Running a record mode of pentab")
-  .action(() => {
-    const child = exec(command, {
-      env: { ...process.env, NODE_ENV: "production", RECORD: "record" },
-      cwd,
-    });
-
-    childHandler(child);
-  });
-
-let buildAttempt = false;
-
-function build() {
-  if (!buildAttempt) {
-    buildAttempt = true;
-    console.log(
-      `[${chalk.hex("#D1070A")(
-        "Error"
-      )}] Could not find a production build, in an attempt to build it.....`
-    );
-
-    const child = exec("npm run build", {
-      cwd,
-    });
-
-    child.stdout.pipe(process.stdout);
-    child.stderr.pipe(process.stderr);
-
-    child.on("exit", (code) => {
-      if (code === 0) {
-        console.log(`[${chalk.green("Success")}] Success building next-pentab`);
-      }
-    });
-  }
-}
-
-function childHandler(child) {
-  child.stdout.on("data", (text) => {
-    if (text.includes("next-pentab")) listen();
-    if (text.includes("[Socket]")) {
-      const event = text.split("[Socket]")[1].trim();
-
-      console.log(`[${chalk.hex("#FDD798")("Socket")}] ${event}`);
-    }
-  });
-
-  child.stderr.on("data", (text) => {
-    if (text.includes("Could not find a production build")) {
-      build();
-    }
-  });
-}
+  .action(() => record());
 
 const parsed = program.parse(process.argv);
 if (
