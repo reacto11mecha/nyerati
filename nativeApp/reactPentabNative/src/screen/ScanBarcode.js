6f7a01dd2f6e60f70d48aa7d88eba6c93e2ad050
Ezra Khairan Permana <ezrakhairan123@gmail.com>
2021-06-03 19:17:41 +0700

    Add barcode scan function and send data via UDP protocol

--- nativeApp/reactPentabNative/src/screen/ScanBarcode.js
+++ nativeApp/reactPentabNative/src/screen/ScanBarcode.js
@@ -1,20 +1,44 @@
-import React, {useRef, useContext, useEffect, useCallback} from 'react';
+import React, {
+  useRef,
+  useContext,
+  useEffect,
+  useState,
+  useCallback,
+} from 'react';
 import {RNCamera} from 'react-native-camera';
 
 import {UdpContext} from '../context/udp';
 
 export default function ScanBarcode() {
-  const {socket} = useContext(UdpContext);
+  const {init, socket} = useContext(UdpContext);
+  const [remote, setRemote] = useState('0.0.0.0');
   const camera = useRef(null);
 
   const onBarcode = useCallback(({barcodes}) => {
-    barcodes.forEach(barcode => console.warn(barcode.data));
+    barcodes.forEach(barcode => {
+      try {
+        const data = JSON.parse(barcode.data);
+        const keys = Object.keys(data);
+
+        if (!keys.includes('errorCode')) {
+          if (keys.includes('connections') && keys.includes('port')) {
+            const {ip} = data.connections[0]; // initial connection
+
+            init(data.port, ip);
+            setRemote(ip);
+          }
+        }
+      } catch (e) {
+        console.error(e);
+      }
+    });
   });
 
   useEffect(() => {
     function onMessage(msg, sender) {
-      console.warn(msg);
-      console.warn(sender);
+      if (sender.address === remote) {
+        // navigate to other screen
+      }
     }
 
     socket.on('message', onMessage);
