5a451e7b2e1180afd550baf5efdc3f6af5fea9f8
Ezra Khairan Permana <ezrakhairan123@gmail.com>
2021-06-03 19:14:55 +0700

    Centering button

--- nativeApp/reactPentabNative/src/screen/Home.js
+++ nativeApp/reactPentabNative/src/screen/Home.js
@@ -1,10 +1,15 @@
 import React from 'react';
-import {View, Text, Button} from 'react-native';
+import {View, Button} from 'react-native';
 
 export default function Home({navigation}) {
   return (
-    <View style={{flex: 1}}>
-      <Text style={{fontSize: 18}}>Plz Scan the barcode</Text>
+    <View
+      style={{
+        flex: 1,
+        padding: 20,
+        flexDirection: 'column',
+        justifyContent: 'center',
+      }}>
       <Button title="Scan" onPress={() => navigation.navigate('ScanBarcode')} />
     </View>
   );
