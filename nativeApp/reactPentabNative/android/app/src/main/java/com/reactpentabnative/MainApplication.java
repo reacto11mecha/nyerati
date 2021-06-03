a85d1f2216f3ae1ea5375d6c1d2c53473730dedb
Ezra Khairan Permana <ezrakhairan123@gmail.com>
2021-06-03 12:50:27 +0700

    Install and linking react-native-udp

--- nativeApp/reactPentabNative/android/app/src/main/java/com/reactpentabnative/MainApplication.java
+++ nativeApp/reactPentabNative/android/app/src/main/java/com/reactpentabnative/MainApplication.java
@@ -4,6 +4,7 @@ import android.app.Application;
 import android.content.Context;
 import com.facebook.react.PackageList;
 import com.facebook.react.ReactApplication;
+import com.tradle.react.UdpSocketsModule;
 import org.reactnative.camera.RNCameraPackage;
 import com.facebook.react.ReactInstanceManager;
 import com.facebook.react.ReactNativeHost;
