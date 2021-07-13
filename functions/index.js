db0377a7f17a0f1840613ab6cd486604ac59783c
Ezra Khairan Permana <ezrakhairan123@gmail.com>
2021-07-13 17:47:48 +0700

    Separate socket io server to different file

--- functions/index.js
+++ functions/index.js
@@ -1,11 +1,13 @@
 const consoleListen = require("./consoleListen");
 const parsecoord = require("./parsecoord");
 const writer = require("./writer");
+const socket = require("./socket");
 const udp = require("./udp");
 
 module.exports = {
   consoleListen,
   parsecoord,
   writer,
+  socket,
   udp,
 };
