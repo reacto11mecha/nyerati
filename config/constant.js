bda8ad744d6f9748ead1ee991c7b858be9276baf
Ezra Khairan Permana <ezrakhairan123@gmail.com>
2021-07-13 17:37:30 +0700

    New dev constant variable

--- config/constant.js
+++ config/constant.js
@@ -1,6 +1,7 @@
 const path = require("path");
 
 const port = parseInt(process.env.PORT, 10) || 3000;
+const dev = process.env.NODE_ENV !== "production";
 
 const isRecording = process.env.RECORD === "record";
 const recordFolder = path.join(__dirname, "..", "record");
@@ -8,6 +9,7 @@ const recordText = path.join(recordFolder, "coord.txt");
 const recordJson = path.join(recordFolder, "convert.json");
 
 module.exports = {
+  dev,
   port,
   isRecording,
   recordFolder,
