ae246dfb5de431aae8efef23039c3fe386d63dc1
Ezra Khairan Permana <ezrakhairan123@gmail.com>
2021-12-20 14:14:43 +0700

    fix: wrap program in handler then because promise pending issue

--- packages/nyudp/cli.js
+++ packages/nyudp/cli.js
@@ -2,24 +2,26 @@
 const program = require("commander");
 const packageData = require("./package.json");
 
-const { run } = require("./lib/handler")(__dirname);
+const handler = require("./lib/handler")(__dirname);
 
 program.version(packageData.version).name(Object.keys(packageData.bin)[0]);
 
-program
-  .command("run")
-  .alias("r")
-  .option("--port <port>", "Change port to specific port")
-  .description("UDP/datagram mode only")
-  .action(run(packageData));
+handler.then(({ run }) => {
+  program
+    .command("run")
+    .alias("r")
+    .option("--port <port>", "Change port to specific port")
+    .description("UDP/datagram mode only")
+    .action(run(packageData));
 
-const parsed = program.parse(process.argv);
-if (
-  !(
-    parsed.args &&
-    parsed.args.length > 0 &&
-    typeof (parsed.args[0] === "object")
-  )
-) {
-  program.help();
-}
+  const parsed = program.parse(process.argv);
+  if (
+    !(
+      parsed.args &&
+      parsed.args.length > 0 &&
+      typeof (parsed.args[0] === "object")
+    )
+  ) {
+    program.help();
+  }
+});
