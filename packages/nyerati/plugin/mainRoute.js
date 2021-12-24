1305831ad3b13b20a2092cdf8a0578b86f7b3013
Ezra Khairan Permana <ezrakhairan123@gmail.com>
2021-12-24 08:41:23 +0700

    feat(nyerati): use `fastify-cors` for cors handling

--- packages/nyerati/plugin/mainRoute.js
+++ packages/nyerati/plugin/mainRoute.js
@@ -2,8 +2,14 @@ const fp = require("fastify-plugin");
 const path = require("path");
 
 const web = require("@nyerati/web-interface");
+const connectivity = require("./connectivity");
 
 module.exports = fp((fastify, opts, done) => {
+  fastify.register(require("fastify-cors"), {
+    origin: connectivity.mainConfig.cors.map((cors) => cors.replace(/\/$/, "")),
+    methods: connectivity.mainConfig.methods,
+  });
+
   fastify.register(require("fastify-compress"));
 
   fastify.register(require("fastify-static"), {
