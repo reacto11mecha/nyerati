7d93fc6097e2a522bcf046f536d18487157f52f1
Ezra Khairan Permana <ezrakhairan123@gmail.com>
2021-12-24 21:02:37 +0700

    feat(web-interface): add socket io status connected or not

--- packages/web-interface/src/App/Components/TouchArea/index.jsx
+++ packages/web-interface/src/App/Components/TouchArea/index.jsx
@@ -1,19 +1,37 @@
-import { onMount, onCleanup } from "solid-js";
+import {
+  onMount,
+  onCleanup,
+  createSignal,
+  createEffect,
+  Switch,
+  Match,
+} from "solid-js";
 import styles from "./TouchArea.module.css";
 import { io } from "socket.io-client";
 import screenfull from "screenfull";
 
+import useColorModeValue from "../../Hook/useColorModeValue";
+
 const { DEV: dev } = import.meta.env;
 
-function TouchArea() {
+export default function TouchArea() {
+  const bg = {
+    na: useColorModeValue("#A0AEC0", "#4A5568"),
+    connected: useColorModeValue("#38A169", "#276749"),
+    disconnected: useColorModeValue("#E53E3E", "#822727"),
+  };
+
+  const [connection, setConnection] = createSignal(null);
+  const [bgStatus, setBgStatus] = createSignal(bg.na());
+
   let touchArea;
   let socket;
 
+  const connectionSetter = () => setConnection(socket.connected);
   const toggleFullScreen = (e) => {
     e.target.blur();
     screenfull.toggle(touchArea, { navigationUI: "hide" });
   };
-
   const handler = (evt) =>
     void socket.emit("touch", {
       x: evt.touches[0].clientX / touchArea.clientWidth,
@@ -23,7 +41,7 @@ function TouchArea() {
   onMount(() => {
     let supportsPassive = false;
     try {
-      var opts = Object.defineProperty({}, "passive", {
+      let opts = Object.defineProperty({}, "passive", {
         get: function () {
           supportsPassive = true;
         },
@@ -34,19 +52,30 @@ function TouchArea() {
 
     socket = dev ? io(`http://${location.hostname}:${portDev}`) : io();
 
-    touchArea.addEventListener(
-      "touchstart",
-      handler,
-      supportsPassive ? { passive: true } : false
-    );
-    touchArea.addEventListener(
-      "touchmove",
-      handler,
-      supportsPassive ? { passive: true } : false
-    );
+    socket.on("connect", connectionSetter);
+    socket.on("disconnect", connectionSetter);
+
+    const opt = supportsPassive ? { passive: true } : false;
+    touchArea.addEventListener("touchstart", handler, opt);
+    touchArea.addEventListener("touchmove", handler, opt);
+  });
+
+  createEffect(() => {
+    if (connection() === null) {
+      setBgStatus(bg.na());
+    } else {
+      if (connection()) {
+        setBgStatus(bg.connected());
+      } else {
+        setBgStatus(bg.disconnected());
+      }
+    }
   });
 
   onCleanup(() => {
+    socket.off("connect", connectionSetter);
+    socket.off("disconnect", connectionSetter);
+
     socket.disconnect();
 
     touchArea.removeEventListener("touchstart", handler);
@@ -66,12 +95,21 @@ function TouchArea() {
             <h3 className={styles.touchAndDrag}>Touch and drag here</h3>
           </div>
           <div className={styles.bottomSpace}>
-            <button onClick={toggleFullScreen}>Toggle Full Screen</button>
+            <button onClick={toggleFullScreen}>Toggle Fullscreen</button>
+            <button
+              onClick={(e) => e.target.blur()}
+              style={`background-color: ${bgStatus()}`}
+              className={styles.statusInfo}
+            >
+              Status:{" "}
+              <Switch fallback={"N/A"}>
+                <Match when={connection()}>CONNECTED</Match>
+                <Match when={connection() === false}>DISCONNECTED</Match>
+              </Switch>
+            </button>
           </div>
         </div>
       </div>
     </div>
   );
 }
-
-export default TouchArea;
