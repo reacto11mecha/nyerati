8b504c5610aefb0ef0cbb5ce9d3a83ad840edadc
Ezra Khairan Permana <ezrakhairan123@gmail.com>
2021-12-26 00:23:30 +0700

    feat(web-interface): create functionality in `/replay` page

--- packages/web-interface/src/App/Components/ReplayList/index.jsx
+++ packages/web-interface/src/App/Components/ReplayList/index.jsx
@@ -1,15 +1,65 @@
-import { createSignal, onMount } from "solid-js";
+import { createSignal, onMount, Switch, Match, For } from "solid-js";
+import { Link } from "solid-app-router";
+import styles from "./ReplayList.module.css";
+
+import useColorModeValue from "../../Hook/useColorModeValue";
 
 const serverPath = import.meta.env.DEV
   ? "http://localhost:3000"
   : location.origin;
 
 export default function ReplayList() {
+  const [files, setFiles] = createSignal(null);
+
+  const outerBorder = useColorModeValue("#aaa", "black");
+  const bottomBorder = useColorModeValue("#aaa", "firebrick");
+
   onMount(() => {
     fetch(`${serverPath}/api/lists`)
       .then((res) => res.json())
-      .then(console.log);
+      .then((data) => setFiles(data));
   });
 
-  return <p>Replay List</p>;
+  return (
+    <div className={styles.mainContainer}>
+      <div>
+        <article
+          class={`card ${styles.cardList}`}
+          style={`border-color: ${outerBorder()}`}
+        >
+          <header style={`border-color: ${bottomBorder()}`}>
+            <h3 className={styles.fileListHeading}>File List</h3>
+          </header>
+          <footer>
+            <Switch fallback={"Unexpected Error"}>
+              <Match when={files() === null}>Loading.....</Match>
+              <Match when={Array.isArray(files()) && files().length < 1}>
+                No recorded data found so far
+              </Match>
+              <Match when={Array.isArray(files()) && files().length > 0}>
+                <ol>
+                  <For each={files()} fallback={<div>Processing data...</div>}>
+                    {(item) => {
+                      const replaceNameFile = item.nameFile.replace(
+                        ".json",
+                        ""
+                      );
+
+                      return (
+                        <li>
+                          <Link href={`/replay/${replaceNameFile}`}>
+                            {replaceNameFile}
+                          </Link>
+                        </li>
+                      );
+                    }}
+                  </For>
+                </ol>
+              </Match>
+            </Switch>
+          </footer>
+        </article>
+      </div>
+    </div>
+  );
 }
