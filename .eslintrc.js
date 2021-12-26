d7eaedc33c6a1bb15248d1464b58380317c0952c
Ezra Khairan Permana <ezrakhairan123@gmail.com>
2021-12-26 08:07:47 +0700

    test: update eslint config for react js

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,14 +1,30 @@
+const websitePackageData = require("./packages/website/package.json");
+
 module.exports = {
   env: {
     commonjs: true,
     es2021: true,
     node: true,
   },
-  plugins: ["solid"],
-  extends: ["eslint:recommended", "prettier", "plugin:solid/recommended"],
+  plugins: ["solid", "react"],
+  extends: [
+    "eslint:recommended",
+    "prettier",
+    "plugin:solid/recommended",
+    "plugin:react/recommended",
+    "plugin:react/jsx-runtime",
+  ],
   parserOptions: {
     ecmaVersion: 13,
     sourceType: "module",
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
+  settings: {
+    react: {
+      version: websitePackageData.dependencies.react,
+    },
   },
   rules: {},
 };
