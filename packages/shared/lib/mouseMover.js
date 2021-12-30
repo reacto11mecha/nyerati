6c794723e2b19c55f71aded69bb2ca33d5e0b800
Ezra Khairan Permana <ezrakhairan123@gmail.com>
2021-12-30 10:57:54 +0700

    perf(nyerati): separate cursor mover from main process

--- packages/shared/lib/mouseMover.js
+++ packages/shared/lib/mouseMover.js
@@ -1,34 +1,35 @@
 const fs = require("fs");
 const path = require("path");
-const robot = require("robotjs");
 const { fork } = require("child_process");
 
-const { width: COMPwidth, height: COMPheight } = robot.getScreenSize();
+const moveMouseWrapper =
+  ({ config }, process) =>
+  () => {
+    const mover = fork("mover", {
+      cwd: path.join(__dirname, "../functions"),
+    });
 
-const moveMouseWrapper = ({ config }, process) => () => {
-  if (!config.constant.isRecording)
-    return (data) =>
-      void robot.moveMouse(data.x * COMPwidth, data.y * COMPheight);
+    if (!config.constant.isRecording) return (data) => mover.send(data);
 
-  const recordText = config.constant.recordText;
-  const writer = fork("writer", {
-    cwd: path.join(__dirname, "../functions"),
-    env: { ...process.env, recordText },
-  });
+    const recordText = config.constant.recordText;
+    const writer = fork("writer", {
+      cwd: path.join(__dirname, "../functions"),
+      env: { ...process.env, recordText },
+    });
 
-  if (!fs.existsSync(config.constant.mainDir))
-    fs.mkdirSync(config.constant.mainDir);
+    if (!fs.existsSync(config.constant.mainDir))
+      fs.mkdirSync(config.constant.mainDir);
 
-  if (!fs.existsSync(recordText)) {
-    if (!fs.existsSync(config.constant.recordFolder))
-      fs.mkdirSync(config.constant.recordFolder);
-    fs.writeFileSync(recordText, "");
-  }
+    if (!fs.existsSync(recordText)) {
+      if (!fs.existsSync(config.constant.recordFolder))
+        fs.mkdirSync(config.constant.recordFolder);
+      fs.writeFileSync(recordText, "");
+    }
 
-  return (data) => {
-    robot.moveMouse(data.x * COMPwidth, data.y * COMPheight);
-    writer.send({ ...data, d: Date.now() });
+    return (data) => {
+      mover.send(data);
+      writer.send({ ...data, d: Date.now() });
+    };
   };
-};
 
 module.exports = moveMouseWrapper;
