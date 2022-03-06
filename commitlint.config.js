0cb685ede904c06b575b1c14df6d0d743687951a
Ezra Khairan Permana <ezrakhairan123@gmail.com>
2022-03-06 14:10:05 +0700

    fix: possible fix for `deps` commitlint scope

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,6 +1,15 @@
+const { getPackages } = require("@commitlint/config-lerna-scopes").utils;
+
+const additionalScope = ["deps"];
+
 module.exports = {
-  extends: [
-    "@commitlint/config-conventional",
-    "@commitlint/config-lerna-scopes",
-  ],
+  extends: ["@commitlint/config-conventional"],
+  rules: {
+    "scope-enum": (ctx) =>
+      getPackages(ctx).then((packages) => [
+        2,
+        "always",
+        [...additionalScope, ...packages],
+      ]),
+  },
 };
